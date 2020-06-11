fc89ade0c44f3aa8c66379ea4782a0117a8b82dc
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-11 19:14:41 -0400

    Added search button and began coding functionality.

--- Develop/script.js
+++ Develop/script.js
@@ -11,29 +11,54 @@ function getLocation() {
         var todayLocalURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${APIKey}`
         var forecastLocalURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly&units=metric&appid=${APIKey}`;
 
-        //Fetch local current weather using geolocation.       
-        function fetchData(URL) {
-            fetch (URL, {
-                method: "GET",
-            })
-            .then(response => response.json())
-            .then(data => {displayData(data), displayUV(data)})
-        }
-        //Display local current weather in current weather box.
-        function displayData(data) {
-            console.log(data);
-            document.querySelector("#icon").innerHTML = `<img src='http://openweathermap.org/img/wn/${data.weather[0].icon}.png'>`;
-            document.querySelector("#cityName").innerHTML = `${data.name}`;
-            document.querySelector("#temperature").innerHTML = `Current Temperature: ${data.main.temp}`;
-            document.querySelector("#windSpeed").innerHTML = `Wind Speed: ${data.wind.speed}`;
-        }
-        fetchData(todayLocalURL);
-        
-        function displayUV(data) {
-            console.log(data)
-            document.querySelector("#UVindex").innerHTML = `${data.daily[0].uvi}`;
-        }
-        fetchData(UVindex);
+        displayData(fetchData(todayLocalURL));
+        displayUV(fetchData(UVindex));  
     })
 }
+
+//Fetch local current weather using geolocation.       
+function fetchData(URL) {
+	var result;
+    fetch (URL, {
+        method: "GET",
+    })
+    .then(response => response.json())
+    .then(data => result = data);
+	return result;
+}
+// var result = fetchData(URL);
+// displayData(result);
+
+//Display local current weather in current weather box.
+function displayData(data) {
+    console.log(data);
+    document.querySelector("#icon").innerHTML = `<img src='https://openweathermap.org/img/wn/${data.weather[0].icon}.png'>`;
+    document.querySelector("#cityName").innerHTML = `${data.name}`;
+    document.querySelector("#temperature").innerHTML = `Current Temperature: ${data.main.temp}`;
+    document.querySelector("#windSpeed").innerHTML = `Wind Speed: ${data.wind.speed}`;
+}
+//Display UV Index        
+function displayUV(data) {
+    document.querySelector("#UVindex").innerHTML = `${data.daily[0].uvi}`;
+}
 getLocation();
+
+
+
+// function fetchData(URL) {
+//     fetch (URL, {
+//         method: "GET",
+//     })
+//     .then(response => response.json())
+//     .then(data => {displayData(data); displayUV(data);})
+// }
+
+//----------------------------------------------------------------------------------------------------
+var searchCityURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+var searchBtn = document.querySelector("#searchBtn");
+searchBtn.addEventListener("click", searchCity)
+
+function searchCity() {
+    var city = document.querySelector("#cityInput").value;
+}
